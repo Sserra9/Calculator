85852e91a7c9af6eef9d42a5c3ef23e3f930cf06
Sean Serra <69557379+Sserra9@users.noreply.github.com>
2020-09-17 19:54:49 -0400

    Update CalculatorApp.py

--- CalculatorApp.py
+++ CalculatorApp.py
@@ -6,29 +6,31 @@ root.title("Simple Calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+
 def button_click(number):
     current = e.get()
     e.delete(0, END)
     e.insert(0, str(current) + str(number))
 
+    
 def button_clear():
     e.delete(0, END)
 
+    
 def button_add():
     first_number = e.get()
     global f_num
     f_num = int(first_number)
     e.delete(0, END)
 
+    
 def button_equals():
     second_number = e.get()
     e.delete(0, END)
     e.insert(0, f_num + int(second_number))
 
 
- 
 #define the buttons 
-
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
 button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
